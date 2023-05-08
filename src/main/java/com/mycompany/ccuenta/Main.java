4378638a7d50aeaa1ae8ee331e7c49687bd9f277
r00lse <libuntux@gmail.com>
2023-05-08 17:28:24 +0200

    Update Main.java

--- src/main/java/com/mycompany/ccuenta/Main.java
+++ src/main/java/com/mycompany/ccuenta/Main.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package com.mycompany.ccuenta;
-
 /**
- *
- * @author ED05
+ * Clase principal 
+ * @author F javier Mendez
  */
 public class Main {
 
